d5311a209459b3f3aba68fbddeaee2249de9b0d2
Farah Jabeen <farahjabeen024@gmail.com>
2026-03-13 10:49:07 +0500

    completed

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask 
+from flask import Flask, render_template,request,json
+from src.pipeline.prediction_pipeline import PredictionPipeline, CustomClass
+'''
 from src.logger import logging
 from src.exception import CustomException
 import os, sys
+'''
 
 #create object of flask class and pass the name of the module as argument
 app = Flask(__name__)
@@ -11,6 +14,40 @@ app = Flask(__name__)
 #POST: This method is used to send data to the server, often via a form submission, to create or update a resource.
 @app.route('/', methods=['GET', 'POST'])
 
+def prediction_data():
+    if request.method == "GET":
+        return render_template("home.html")
+    else:
+        data = CustomClass(
+            age = int(request.form.get("age")),
+            workclass = int(request.form.get("workclass")),
+            education_num = int(request.form.get("education_num")),
+            marital_status = int(request.form.get("marital_status")),
+            occupation = int(request.form.get("occupation")),
+            relationship = int(request.form.get("relationship")),
+            race = int(request.form.get("race")),
+            sex = int(request.form.get("sex")),
+            capital_gain = int(request.form.get("capital_gain")),
+            capital_loss = int(request.form.get("capital_loss")),
+            hours_per_week = int(request.form.get("hours_per_week")),
+            native_country = int(request.form.get("native_country"))
+        )
+        
+        final_data = data.get_data_DataFrame()
+        pipeline_prediction = PredictionPipeline()
+        pred = pipeline_prediction.predict(final_data)
+        result = pred
+
+        #write condition for 0 and 1 according to problem statement
+        if result == 0:
+            return render_template("results.html", final_result = "your yearly income is less than and equal $50K:{}".format(result))
+        elif result == 1:
+            return render_template("results.html", final_result = "your yearly income is more than and equal $50K:{}".format(result))
+
+if __name__ ==  "__main__":
+    app.run(host = "0.0.0.0", debug = True)
+            
+'''
 def index():
     try:
         raise Exception("We are testing our custom exception")
@@ -23,6 +60,7 @@ def index():
         abc = CustomException(e, sys)
         logging.error(abc.error_message)
         return "Welcome to ML pipeline"
+        '''
 
 if __name__ == "__main__":
    # app.run(debug=True)
